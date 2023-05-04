f92d6657903a5b9f73b8b838e6bc7a83894fba0b
mariangelkruiz <marianruizdev@gmail.com>
2023-05-04 15:15:03 -0300

    arquitectura del proyecto

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express= require('express');
+const bodyParser = require("body-parser")
 
 const app = express();
+app.use(bodyParser.json());
+
 
 app.get("/", (req, res)=> {
     res.send("hello world");
 });
 
 app.listen(3001, ()=> {
-    console.log("server running on port", 3001);
+    console.log("server running on port http://localhost:3001/");
 });
